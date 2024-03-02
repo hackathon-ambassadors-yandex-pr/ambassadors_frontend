8bcb65ffafb7f0d591e6feb2c9ec75986ceed293
AndreiZhura <zhura2296@yandex.ru>
2024-03-02 16:10:31 +0300

    table and page scroll

--- src/components/Ambassadors/Ambassadors.tsx
+++ src/components/Ambassadors/Ambassadors.tsx
@@ -3,6 +3,8 @@ import bluebell from '../../../src/images/notification.svg';
 import manager from '../../images/manager_photo.svg';
 import mail from '../../images/mail.svg';
 import filter from '../../images/filters.svg';
+import arrows from '../../images/container_vector_icon.png';
+import telegramm from '../../images/telegram.svg';
 import { NavLink } from 'react-router-dom';
 
 function Ambassadors() {
@@ -49,8 +51,107 @@ function Ambassadors() {
           <img className="ambasadors__button" src={mail} />
           <img className="ambasadors__button" src={filter} />
         </div>
-        <div className="table">
-          <div className="table__top"></div>
+      </div>
+      <div className="table">
+        <div className="table__top">
+          <input type="checkbox" className="table__checkbox" />
+          <p className="table__text">Фамилия Имя</p>
+          <img className="table__arrows" src={arrows} />
+          <p className="table__text">Промокод</p>
+          <p className="table__text">Дата</p>
+          <img className="table__arrows" src={arrows} />
+          <p className="table__text">Статус</p>
+          <p className="table__text">Контент</p>
+          <img className="table__arrows" src={arrows} />
+          <p className="table__text">Действие</p>
+          <img className="table__arrows" src={arrows} />
+          <p className="table__text">телеграмм</p>
+        </div>
+        <div className="items">
+          <input type="checkbox" className="items__checkbox" />
+          <p className="items__name">Иванов Иван</p>
+          <p className="items__promocod"> STEPA</p>
+          <p className="items__date">15.12.24</p>
+          <p className="items__status">Новый</p>
+          <p className="items__content">1</p>
+          <p className="items__action">Новый амбасадор</p>
+          <img className="items__telegramm" src={telegramm} />
+        </div>
+        <div className="items">
+          <input type="checkbox" className="items__checkbox" />
+          <p className="items__name">Иванов Иван</p>
+          <p className="items__promocod"> STEPA</p>
+          <p className="items__date">15.12.24</p>
+          <p className="items__status">Новый</p>
+          <p className="items__content">1</p>
+          <p className="items__action">Новый амбасадор</p>
+          <img className="items__telegramm" src={telegramm} />
+        </div>
+        <div className="items">
+          <input type="checkbox" className="items__checkbox" />
+          <p className="items__name">Иванов Иван</p>
+          <p className="items__promocod"> STEPA</p>
+          <p className="items__date">15.12.24</p>
+          <p className="items__status">Новый</p>
+          <p className="items__content">1</p>
+          <p className="items__action">Новый амбасадор</p>
+          <img className="items__telegramm" src={telegramm} />
+        </div>
+        <div className="items">
+          <input type="checkbox" className="items__checkbox" />
+          <p className="items__name">Иванов Иван</p>
+          <p className="items__promocod"> STEPA</p>
+          <p className="items__date">15.12.24</p>
+          <p className="items__status">Новый</p>
+          <p className="items__content">1</p>
+          <p className="items__action">Новый амбасадор</p>
+          <img className="items__telegramm" src={telegramm} />
+        </div>
+        <div className="items">
+          <input type="checkbox" className="items__checkbox" />
+          <p className="items__name">Иванов Иван</p>
+          <p className="items__promocod"> STEPA</p>
+          <p className="items__date">15.12.24</p>
+          <p className="items__status">Новый</p>
+          <p className="items__content">1</p>
+          <p className="items__action">Новый амбасадор</p>
+          <img className="items__telegramm" src={telegramm} />
+        </div>
+        <div className="items">
+          <input type="checkbox" className="items__checkbox" />
+          <p className="items__name">Иванов Иван</p>
+          <p className="items__promocod"> STEPA</p>
+          <p className="items__date">15.12.24</p>
+          <p className="items__status">Новый</p>
+          <p className="items__content">1</p>
+          <p className="items__action">Новый амбасадор</p>
+          <img className="items__telegramm" src={telegramm} />
+        </div>
+        <div className="items">
+          <input type="checkbox" className="items__checkbox" />
+          <p className="items__name">Иванов Иван</p>
+          <p className="items__promocod"> STEPA</p>
+          <p className="items__date">15.12.24</p>
+          <p className="items__status">Новый</p>
+          <p className="items__content">1</p>
+          <p className="items__action">Новый амбасадор</p>
+          <img className="items__telegramm" src={telegramm} />
+        </div>
+        <div className="items">
+          <input type="checkbox" className="items__checkbox" />
+          <p className="items__name">Иванов Иван</p>
+          <p className="items__promocod"> STEPA</p>
+          <p className="items__date">15.12.24</p>
+          <p className="items__status">Новый</p>
+          <p className="items__content">1</p>
+          <p className="items__action">Новый амбасадор</p>
+          <img className="items__telegramm" src={telegramm} />
+        </div>
+      </div>
+      <div className="page">
+        <div className="page__number">
+          <button className="arrow arrow-left"></button>
+          <button className="arrow arrow-right"></button>
         </div>
       </div>
     </div>
