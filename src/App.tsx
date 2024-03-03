d8a1bc78d71a44c639c957f26d742db5fcdd6f4a
AndreiZhura <zhura2296@yandex.ru>
2024-03-03 19:37:14 +0300

    header

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import './App.scss';
+import Header from './components/header/header';
 import Ambassadors from './components/Ambassadors/Ambassadors';
 import Layout from './components/Layout/Layout';
-import logo from './images/logo.svg';
-import { NavLink } from 'react-router-dom';
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -10,27 +9,7 @@ function App() {
     <>
       <div className="app">
         <div className="page__container">
-          <div className="header">
-            <img src={logo} className="header__logo" />
-            <nav className="navigation">
-              <NavLink to="#" className="navigation__item">
-                Амбассадоры
-              </NavLink>
-              <NavLink to="#" className="navigation__item">
-                Контент
-              </NavLink>
-              <NavLink to="#" className="navigation__item">
-                Мерч
-              </NavLink>
-              <NavLink to="#" className="navigation__item">
-                Бюджет
-              </NavLink>
-              <NavLink to="#" className="navigation__item">
-                Аналитика
-              </NavLink>
-            </nav>
-            <button className="header__exit">Выход</button>
-          </div>
+          <Header />
           <Routes>
             <Route path="/" element={<Layout />} />
             <Route index element={<Ambassadors />} />
