6c811fd72affb2141f945cea54f2d1976e2ae1d4
Denis Manaev <manaewd@yandex.ru>
2024-03-04 22:02:36 +0700

    update styles

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,15 +1,15 @@
 import * as React from 'react';
-// import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
-// import authLogo from "../../images/auth-logo.svg";
+import authLogo from '../../images/auth-logo.svg';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import './Login.scss';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 function Copyright(props: any) {
@@ -46,20 +46,36 @@ export default function Login() {
         <CssBaseline />
         <Box
           sx={{
-            marginTop: 8,
+            marginTop: 38,
             display: 'flex',
             flexDirection: 'column',
             alignItems: 'center',
+            width: '456px',
           }}
         >
-          <Typography component="h1" variant="h5">
+          <div>
+            <a href="" target="_blank">
+              <img src={authLogo} className="logo" alt="Логин" />
+            </a>
+          </div>
+          <Typography
+            component="h4"
+            fontFamily="YS Display"
+            color="var(--Main-Black-300, #B5B5B7)"
+          >
             Войдите в аккаунт
           </Typography>
           <Box
             component="form"
             onSubmit={handleSubmit}
             noValidate
-            sx={{ mt: 1 }}
+            sx={{
+              mt: 1,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              width: '390px',
+            }}
           >
             <TextField
               margin="normal"
@@ -70,6 +86,7 @@ export default function Login() {
               name="email"
               autoComplete="email"
               autoFocus
+              sx={{ mt: 3, mb: 0 }}
             />
             <TextField
               margin="normal"
@@ -80,26 +97,50 @@ export default function Login() {
               type="password"
               id="password"
               autoComplete="current-password"
+              sx={{ mt: 3, mb: 4 }}
             />
-            <Button
-              type="submit"
-              fullWidth
-              disableElevation
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Войти
-            </Button>
             <Grid container>
               <Grid item xs>
-                <Link href="#" variant="body2">
+                <Link
+                  underline="none"
+                  fontFamily="YS Display"
+                  fontSize={'20px'}
+                  lineHeight={'24px'}
+                  fontWeight={'400'}
+                  href="#"
+                  variant="caption"
+                  color="var(--Main-Blue-main, #5A9BFF)"
+                >
                   Не помню пароль
                 </Link>
               </Grid>
             </Grid>
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+              }}
+            >
+              <Button
+                type="submit"
+                size="small"
+                disableElevation
+                variant="contained"
+                sx={{
+                  mt: 3,
+                  mb: 2,
+                  width: '248px',
+                  height: '48px',
+                  background: 'var(--Main-Blue-main, #5A9BFF)',
+                }}
+              >
+                Войти
+              </Button>
+            </Box>
           </Box>
         </Box>
-        <Copyright sx={{ mt: 31, mb: 3 }} />
+        <Copyright sx={{ mt: 37, mb: 3 }} />
       </Container>
     </ThemeProvider>
   );
