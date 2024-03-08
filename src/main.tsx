c1c1cc986ce4bfa79fb570515fe09376982f2ca2
Дмитрий Типсин <tipsin.dm@yandex.ru>
2024-03-08 06:21:18 +0300

    fix: Мелкие исправления

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './main.scss';
-import { ThemeProvider } from '@emotion/react';
-import theme from './theme.tsx';
+// import { ThemeProvider } from '@emotion/react';
+// import theme from './theme.tsx';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <ThemeProvider theme={theme}>
-    <React.StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </React.StrictMode>
-  </ThemeProvider>
+  // <ThemeProvider theme={theme}>
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+  // </ThemeProvider>
 );
--- src/theme.tsx
+++ src/theme.tsx
@@ -1,106 +1,106 @@
-import { createTheme, responsiveFontSizes } from '@mui/material';
+// import { createTheme, responsiveFontSizes } from '@mui/material';
 
-const colors = {
-  black: {
-    // '900': '#1a1b22',
-    '700': '#2B2D33',
-    '500': '#797981',
-    '300': '#B5B5B7',
-    '100': '#DDE0E4',
-  },
-  white: '#ffffff',
-  blue: {
-    main: '#5A9BFF',
-    base: '#1D6BF3',
-    bg: '#F1F6FF',
-  },
-  red_error: '#E81818',
-  green_success: '#2AAD27',
-  rose: '#FFDDE5',
-  yellow: '#FFF9D3',
-  green: '#C2E5CE',
-  yellow_status: '#928324',
-  rose_status: '#79182F',
-  green_status: '#0C6B2D',
-  shadow: '#B0BEC55C',
-  bg: '#2B2D3380',
-};
+// const colors = {
+//   black: {
+//     // '900': '#1a1b22',
+//     '700': '#2B2D33',
+//     '500': '#797981',
+//     '300': '#B5B5B7',
+//     '100': '#DDE0E4',
+//   },
+//   white: '#ffffff',
+//   blue: {
+//     main: '#5A9BFF',
+//     base: '#1D6BF3',
+//     bg: '#F1F6FF',
+//   },
+//   red_error: '#E81818',
+//   green_success: '#2AAD27',
+//   rose: '#FFDDE5',
+//   yellow: '#FFF9D3',
+//   green: '#C2E5CE',
+//   yellow_status: '#928324',
+//   rose_status: '#79182F',
+//   green_status: '#0C6B2D',
+//   shadow: '#B0BEC55C',
+//   bg: '#2B2D3380',
+// };
+//
+// const theme = responsiveFontSizes(
+//   createTheme({
+//     palette: {
+//       primary: {
+//         main: colors.blue.main,
+//         // light:#42a5f5
+//         //dark: colors.blue.base,
+//         // contrastText:#fff
+//       },
+//       error: {
+//         main: colors.red_error,
+//       },
+//       success: {
+//         main: colors.green_success,
+//       },
+//       text: {
+//         // primary:,
+//         // secondary:,
+//         // disabled
+//       },
+//     },
+//     typography: {
+//       fontFamily: '"YS Display", "Helvetica", "Arial", sans-serif',
+//       h1: {
+//         fontSize: 34,
+//         fontWeight: 400,
+//         lineHeight: 1.17647,
+//       },
+//       h2: {
+//         fontSize: 28,
+//         fontWeight: 400,
+//         lineHeight: 1.28571,
+//       },
+//       h3: {
+//         fontSize: 24,
+//         fontWeight: 400,
+//         lineHeight: 1.33333,
+//       },
+//       h4: {
+//         fontSize: 20,
+//         fontWeight: 400,
+//         lineHeight: 1.2,
+//       },
+//       subtitle1: {
+//         fontFamily: '"YS Text", "Helvetica", "Arial", sans-serif',
+//         fontSize: 13,
+//         fontWeight: 400,
+//         lineHeight: 1.23077,
+//       },
+//       subtitle2: {
+//         fontFamily: '"YS Text", "Helvetica", "Arial", sans-serif',
+//         fontSize: 11,
+//         fontWeight: 400,
+//         lineHeight: 1.09091,
+//       },
+//       body1: {
+//         fontFamily: '"YS Text", "Helvetica", "Arial", sans-serif',
+//         fontSize: 16,
+//         fontWeight: 400,
+//         lineHeight: 1.25,
+//       },
+//       body2: {
+//         fontFamily: '"YS Text", "Helvetica", "Arial", sans-serif',
+//         fontSize: 14,
+//         fontWeight: 400,
+//         lineHeight: 1.42857,
+//       },
+//       caption: {
+//         fontFamily: '"YS Text", "Helvetica", "Arial", sans-serif',
+//         fontSize: 18,
+//         fontWeight: 400,
+//         lineHeight: 1.33333,
+//       },
+//     },
+//   })
+// );
 
-const theme = responsiveFontSizes(
-  createTheme({
-    palette: {
-      primary: {
-        main: colors.blue.main,
-        // light:#42a5f5
-        dark: colors.blue.base,
-        // contrastText:#fff
-      },
-      error: {
-        main: colors.red_error,
-      },
-      success: {
-        main: colors.green_success,
-      },
-      text: {
-        // primary:,
-        // secondary:,
-        // disabled
-      },
-    },
-    typography: {
-      fontFamily: '"YS Display", "Helvetica", "Arial", sans-serif',
-      h1: {
-        fontSize: 34,
-        fontWeight: 400,
-        lineHeight: 1.17647,
-      },
-      h2: {
-        fontSize: 28,
-        fontWeight: 400,
-        lineHeight: 1.28571,
-      },
-      h3: {
-        fontSize: 24,
-        fontWeight: 400,
-        lineHeight: 1.33333,
-      },
-      h4: {
-        fontSize: 20,
-        fontWeight: 400,
-        lineHeight: 1.2,
-      },
-      subtitle1: {
-        fontFamily: '"YS Text", "Helvetica", "Arial", sans-serif',
-        fontSize: 13,
-        fontWeight: 400,
-        lineHeight: 1.23077,
-      },
-      subtitle2: {
-        fontFamily: '"YS Text", "Helvetica", "Arial", sans-serif',
-        fontSize: 11,
-        fontWeight: 400,
-        lineHeight: 1.09091,
-      },
-      body1: {
-        fontFamily: '"YS Text", "Helvetica", "Arial", sans-serif',
-        fontSize: 16,
-        fontWeight: 400,
-        lineHeight: 1.25,
-      },
-      body2: {
-        fontFamily: '"YS Text", "Helvetica", "Arial", sans-serif',
-        fontSize: 14,
-        fontWeight: 400,
-        lineHeight: 1.42857,
-      },
-      caption: {
-        fontFamily: '"YS Text", "Helvetica", "Arial", sans-serif',
-        fontSize: 18,
-        fontWeight: 400,
-        lineHeight: 1.33333,
-      },
-    },
-  })
-);
-
-export default theme;
+// export default theme;
